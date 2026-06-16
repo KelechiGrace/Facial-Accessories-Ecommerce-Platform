b9da0a842c8dbbf600be88d04c6ff1d04a9011f0
KelechiGrace <kiheme.2203225@stu.cu.edu.ng>
2026-06-16 11:03:23 -0700

    Delete SellerRoute.jsx

--- SellerRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function SellerRoute({ children }) {
-  const user = JSON.parse(localStorage.getItem("user") || "{}");
-  if (!user.email) return <Navigate to="/admin/login" />;
-  if (user.role !== "seller") return <Navigate to="/" />;
-  return children;
-}
